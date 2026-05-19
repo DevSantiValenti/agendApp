e662c1f59a879f4603dfcdfb358f66dc4de8d576
SadSatan <santiagovalentixd@gmail.com>
2026-05-19 11:51:20 -0300

    First commit

---
+++ src/test/java/com/analistas/agendApp/AgendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analistas.agendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
